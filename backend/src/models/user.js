c8e3239b9224669a31b3a885754c395416ba524e
Omer <farukoncu78@gmail.com>
2024-08-08 10:10:57 +0300

    up

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -15,7 +15,7 @@ const userSchema = mongoose.Schema({
         type: String,
         trim: true,
         required: true,
-        unique: true
+        //unique: true
     },
     password: {
         type: String,
